00b24290ee48e472ed5c0196c7b46cf865255526
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2017-10-06 10:27:35 -0300

    Criar getLista e o TestaLista

--- src/br/com/caelum/jdbc/dao/ContatoDao.java
+++ src/br/com/caelum/jdbc/dao/ContatoDao.java
@@ -3,7 +3,11 @@ package br.com.caelum.jdbc.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import br.com.caelum.jdbc.ConnectionFactory;
 import br.com.caelum.jdbc.modelo.Contato;
@@ -31,6 +35,38 @@ public class ContatoDao {
 			stmt.execute();
 			stmt.close();
 		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public List<Contato> getList() {
+		try {
+			List<Contato> contatos = new ArrayList<Contato>();
+			
+			PreparedStatement stmt = this.connection.prepareStatement(
+					"select * from contatos");
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				Contato contato = new Contato();
+				
+				contato.setId(rs.getLong("id"));
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+				
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+				
+				contatos.add(contato);
+			}
+			rs.close();
+			stmt.close();
+			return contatos;
+			
+		}catch(SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
